a7f343682258aa64ac65751eecb12e644d509491
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-27 00:39:47 +0300

    ...

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,8 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
 
 const options = {
   enableTime: true,
@@ -7,16 +10,59 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
-    window.alert("Please choose a date in the future")
+    let selectedUnixTime = selectedDates[0].getTime();
+    let currentUnixTime = Date.now();
+    const timeLeft = selectedUnixTime - currentUnixTime;
+
+    if(selectedUnixTime <= currentUnixTime){
+        Notify.failure('Please choose a date in the future');
+    }else{
+        refs.startBtn.toggleAttribute('disabled');
+        return convertMs(timeLeft)
+    };
   },
 };
 
+
+
 const refs = {
     input: document.querySelector('#datetime-picker'),
-}
+    startBtn: document.querySelector('button[data-start]'),
+    days: document.querySelector('span[data-days]'),
+    hours: document.querySelector('span[data-hours]'),
+    minutes: document.querySelector('span[data-minutes]'),
+    seconds: document.querySelector('span[data-seconds]'),
+};
 
 const fp =  flatpickr(refs.input, options);
+
+refs.startBtn.toggleAttribute('disabled');
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+
+
 
 
 
