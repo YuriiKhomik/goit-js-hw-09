ffed2760c1efe82f4735eaa8147533c6cd5f6076
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-26 16:03:06 +0300

    task1+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+const chngBgColorInterval = 1000;
+let intervalId = 0;
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+refs.stopBtn.toggleAttribute('disabled');
+
+function onStartBtnClick(){
+
+    refs.startBtn.toggleAttribute('disabled');
+    refs.stopBtn.toggleAttribute('disabled');
+
+    intervalId = setInterval(() => {
+        const color = getRandomHexColor()
+        refs.body.style.backgroundColor = color;
+    }, chngBgColorInterval);
+};
+
+function onStopBtnClick(){
+    clearInterval(intervalId);
+
+    refs.startBtn.toggleAttribute('disabled');
+    refs.stopBtn.toggleAttribute('disabled');
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
