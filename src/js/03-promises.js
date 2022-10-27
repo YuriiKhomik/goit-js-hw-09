f781ab5030f97dd91d49b06f5cfb0ea74675189d
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-27 20:10:47 +0300

    task3+

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,52 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
-// const refs = {
-//   delay: document.querySelector('input[name="delay"]'),
-//   step: document.querySelector('input[name="step"]'),
-//   amount: document.querySelector('input[name="amount"]'),
-//   form: document.querySelector('.form'),
-//   btn:  document.querySelector('.form > button'),
-// }
-
-// // refs.btn.addEventListener('click', onBtnClick);
-// refs.form.addEventListener('input', onFormInput);
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// function onFormInput(e){};
-
-// function onFormSubmit(e){
-//   e.preventDefault();
-//   let inputDelay = refs.delay.value;
-//   const inputStep = refs.step.value;
-//   const inputAmount = refs.amount.value;
-//   let intervalCounter = 0;
-
-//   const intervalId = setInterval(() => {
-//     intervalCounter += 1;
-//     inputDelay += inputStep;
-    
-//     if(intervalCounter >= inputAmount){
-//       clearInterval(intervalId);
-//     }
-
-//     createPromise({intervalCounter, inputDelay})
-
-//   }, inputDelay);
-// };
-
-// function createPromise({intervalCounter, inputDelay}) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     Notify.success(`✅ Fulfilled promise ${intervalCounter} in ${inputDelay}ms`);
-//     console.log(`✅ Fulfilled promise ${intervalCounter} in ${inputDelay}ms`);
-//   } else {
-//     Notify.failure('`❌ Rejected promise ${intervalCounter} in ${inputDelay}ms`');
-//     console.log(`❌ Rejected promise ${intervalCounter} in ${inputDelay}ms`)
-//   }
-// };
-
-
 const refs = {
   form: document.querySelector('.form'),
 }
